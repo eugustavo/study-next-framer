1ae64e28f3f7799aeb73fddf60cb21923acd268f
eugustavo <gustavo.ifto@gmail.com>
2020-09-19 18:49:44 -0300

    Add initial data

---
+++ src/data/index.ts
@@ -0,0 +1,10 @@
+export const items = [
+  {
+    id: 1,
+    title: 'Tasks: Cafe da manhã',
+    description: 'Fazer o cafe da manhã'
+  },
+  { id: 2, title: 'Tasks: Almoço', description: 'Fazer o almoço' },
+  { id: 3, title: 'Tasks: Lanche', description: 'Fazer o lanche' },
+  { id: 4, title: 'Tasks: Janta', description: 'Fazer a janta' }
+]
