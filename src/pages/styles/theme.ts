f0a3dd914d1f86a9b78f0b1ad62ec6b82ad4d2ea
eugustavo <gustavo.ifto@gmail.com>
2020-09-19 18:50:08 -0300

    Add cancel color in theme

--- src/pages/styles/theme.ts
+++ src/pages/styles/theme.ts
@@ -3,8 +3,9 @@ const theme = {
     background: '#121214',
     taskBackground: 'rgb(32, 32, 36)',
     text: '#e1e1e6',
-    description: '#cdcdd1',
-    primary: '#8257e6'
+    description: '#cdcdcd',
+    primary: '#8257e6',
+    cancel: '#b50000'
   }
 }
 
